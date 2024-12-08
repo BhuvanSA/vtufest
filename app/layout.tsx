6a80d23a4f09a3e46fc6012c0497c8a28eb093b0
Likhith Reddy Kottapalli <108880762+MrLynx8@users.noreply.github.com>
2024-12-09 00:29:50 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,22 @@
-
+import React, { ReactNode } from "react";
+import Navbar from ".././components/Navbar";
+import Footer from ".././components/Footer";
 import "./globals.css";
 
-
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+// Global layout for pages
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
-      <body
-        className="bg-gray-400"
-      >
-        {children}
+      <head>
+        {/* You can add custom head tags here, like meta tags, link to styles, etc. */}
+      </head>
+      <body>
+        <Navbar />
+        <main className="flex flex-col">{children}</main>
+        <Footer />
       </body>
     </html>
   );
-}
+};
+
+export default Layout;
