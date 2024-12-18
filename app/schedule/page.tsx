d83fc51bae777a456e50d4f836a1e2eddba06242
BhuvanSA <bhuvansaraj2003@gmail.com>
2024-12-18 09:31:43 +0530

    Add spacing above content in About, Events, and Schedule pages

--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 
 const page = () => {
-  return (
-    <div className="p-10">
-      {" "}
-      {/* Adds padding around the entire content */}
-      <h1 className="text-center text-5xl font-bold mb-10 tracking-wide">
-        Event Schedule
-      </h1>
-      <p className="text-center text-3xl leading-relaxed tracking-wider">
-        The detailed schedule will be released soon. Mark your calendars for an
-        unforgettable experience!
-      </p>
-    </div>
-  );
+    return (
+        <div className="p-10">
+            <div className="h-20"></div>{" "}
+            {/* Adds padding around the entire content */}
+            <h1 className="text-center text-5xl font-bold mb-10 tracking-wide">
+                Event Schedule
+            </h1>
+            <p className="text-center text-3xl leading-relaxed tracking-wider">
+                The detailed schedule will be released soon. Mark your calendars
+                for an unforgettable experience!
+            </p>
+        </div>
+    );
 };
 
 export default page;
