df1888aad5ad6e1e01e543bb1866f695ec90093e
BhuvanSA <bhuvansaraj2003@gmail.com>
2025-02-01 13:15:21 +0530

    Remove old comments

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,9 @@
 import React from "react";
 import "./globals.css"; // Adjust the import path as needed
 import Header from "@/components/Background";
-// import Gallery from "@/components/subcomponents/gallery";
-// import Pronities from "@/components/subcomponents/pronities";
-// import Events from "@/components/subcomponents/events";
 
 const Home = () => {
     return (
-
         <div className="bg-background pt-16">
             <Header />
             <div className="relative z-10">
@@ -16,23 +12,68 @@ const Home = () => {
                     <div className="ml-auto ">
                         <div className="lg:w-2/3 mt-10 text-justify mx-auto">
                             {/* <h1 className="text-primary font-bold text-5xl md:text-6xl xl:text-7xl text-center flex flex-col"> */}
-                                <span className="text-primary mt-8 text-black">
-                                    <p style={{ color: "#F4d03f", fontSize: "2.5rem" }}>GLOBAL ACADEMY OF TECHNOLOGY</p>
-                                    <p style={{ color: "#D32f23", fontSize: "1.5rem" }}>GROWING AHEAD OF TIME</p>
-                                    <p style={{ color: "#003366", fontSize: "1.5rem" }}>AN AUTONOMOUS INSTITUTE, AFFILIATED TO VTU</p>
-                                </span>
+                            <span className="text-primary mt-8 text-black">
+                                <p
+                                    style={{
+                                        color: "#F4d03f",
+                                        fontSize: "2.5rem",
+                                    }}
+                                >
+                                    GLOBAL ACADEMY OF TECHNOLOGY
+                                </p>
+                                <p
+                                    style={{
+                                        color: "#D32f23",
+                                        fontSize: "1.5rem",
+                                    }}
+                                >
+                                    GROWING AHEAD OF TIME
+                                </p>
+                                <p
+                                    style={{
+                                        color: "#003366",
+                                        fontSize: "1.5rem",
+                                    }}
+                                >
+                                    AN AUTONOMOUS INSTITUTE, AFFILIATED TO VTU
+                                </p>
+                            </span>
                             {/* </h1> */}
 
                             <h4 className="mt-8 text-black">
-
                                 <p>-- WHO WE ARE</p>
-                                <p>AUTONOMOUS INSTITUTION WITH NAAC-A ACCREDITATION</p>
-                                <p>      Global Academy of Technology is an A-grade college that is counted one amongst the best engineering colleges in Bangalore. 
-                                Along with being equipped with modern technology and the perfect infrastructure, the college has an ambience and culture to accelerate learning.
-                                The Management, Principal and Staff of the institution believe in the overall development of the students and hence encourage them to participate in co-curricular, extra-curricular and sports events.</p>
+                                <p>
+                                    AUTONOMOUS INSTITUTION WITH NAAC-A
+                                    ACCREDITATION
+                                </p>
+                                <p>
+                                    {" "}
+                                    Global Academy of Technology is an A-grade
+                                    college that is counted one amongst the best
+                                    engineering colleges in Bangalore. Along
+                                    with being equipped with modern technology
+                                    and the perfect infrastructure, the college
+                                    has an ambience and culture to accelerate
+                                    learning. The Management, Principal and
+                                    Staff of the institution believe in the
+                                    overall development of the students and
+                                    hence encourage them to participate in
+                                    co-curricular, extra-curricular and sports
+                                    events.
+                                </p>
                                 <p>-- Why is 2025 iconic for GAT?</p>
-                                <p>      This year 2025 marks a momentous milestone as the institution celebrates its Silver Jubilee – 25 years of academic excellence, 
-                                innovation, and transformative education. With a legacy of shaping future leaders and achievers, GAT continues to set benchmarks in higher education, solidifying its position as a premier destination for aspiring engineers and managers</p>
+                                <p>
+                                    {" "}
+                                    This year 2025 marks a momentous milestone
+                                    as the institution celebrates its Silver
+                                    Jubilee – 25 years of academic excellence,
+                                    innovation, and transformative education.
+                                    With a legacy of shaping future leaders and
+                                    achievers, GAT continues to set benchmarks
+                                    in higher education, solidifying its
+                                    position as a premier destination for
+                                    aspiring engineers and managers
+                                </p>
                             </h4>
                             <div className="flex justify-between">
                                 <div className="flex  space-x-2">
