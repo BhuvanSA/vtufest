75c8d0a35ae05242882ca25f4dd22d77dc04094f
dgatclg <darshan1ga21ad016@gmail.com>
2025-02-02 00:09:39 +0530

    Changes for about gat page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,134 +1,156 @@
 "use client";
 import React from "react";
-import { image5, image34, image42 } from "@/components/images/gallery";
 import Image from "next/image";
 import gat from "@/components/images/gat1.jpg";
+// Credential images – update these paths with your actual image files.
+import affiliationImg from "@/components/images/affiliation.png";
+import aicteImg from "@/components/images/aicte.png";
+import recognizedImg from "@/components/images/recognized.png";
 
 const About = () => {
-    return (
-        <div>
-            <div className="overflow-hidden pt-20 pb-12 lg:pt-[120px] lg:pb-[90px] text-[#bbc5c6]">
-                <div className="container mx-auto">
-                    <div className="   md:px-12 xl:px-6">
-                        <div className="relative  ">
-                            <div className="lg:w-2/3 md:text-center  mx-auto">
-                                <h1 className="text-[#bbc5c6] font-bold text-4xl md:text-6xl xl:text-7xl">
-                                    About
-                                    <span className="text-primary text-[#e2af3e]">
-                                        {" "}
-                                        Us.
-                                    </span>
-                                </h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="-mx-4 mt-10 flex flex-wrap items-center justify-between">
-                        <div className="w-full px-4 lg:w-6/12">
-                            <div className="-mx-3 flex items-center sm:-mx-4">
-                                {/* <div className="w-full px-3 sm:px-4 xl:w-1/2"> */}
-                                <div className="relative  z-10 my-4 h-auto max-w-lg transition-all duration-300 rounded-lg cursor-pointer filter ">
-                                    <Image
-                                        src={gat}
-                                        alt=""
-                                        className=" h-[350px] rounded-2xl "
-                                    />
-                                    {/* </div> */}
-                                </div>
-                            </div>
-                        </div>
-                        <div className="w-full px-4 lg:w-1/2 xl:w-5/12">
-                            <div className="mt-10 lg:mt-0">
-                                <p className="text-body-color text-justify mb-8 text-base">
-                                    Global Academy of Technology was established
-                                    in 2001, celebrating 25 years of excellence
-                                    with the monumental objective of promoting
-                                    academic excellence and competence in
-                                    students, especially in the fast-growing
-                                    global domain of Engineering Technology and
-                                    Management. The Institute made remarkable
-                                    progress since its inception in every aspect
-                                    and gained name among the Institutes of
-                                    Technology in the state of Karnataka due to
-                                    the quality of education and training
-                                    provided by its dedicated faculty and the
-                                    infrastructure available.{" "}
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="   md:px-12 xl:px-6">
-                        <div className="relative pt-36 ">
-                            <div className="lg:w-2/3 md:text-center  mx-auto">
-                                <h1 className="text-[#bbc5c6] font-bold text-4xl md:text-6xl xl:text-7xl">
-                                    About
-                                    <span className="text-primary text-[#e2af3e]">
-                                        {" "}
-                                        VTU Youth Fest.
-                                    </span>
-                                </h1>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="-mx-4 mt-10 flex flex-wrap items-center justify-between">
-                        <div className="w-full lg:w-1/2 xl:w-5/12">
-                            <div className="mt-10 lg:mt-0">
-                                <p className="text-body-color mb-8 text-justify text-base">
-                                    VTU Youth Fest attracts participants from
-                                    various colleges across the region. The fest
-                                    features a wide range of events, including
-                                    technical competitions, cultural
-                                    performances, workshops, seminars, and
-                                    sports activities. The technical
-                                    competitions include coding challenges,
-                                    hackathons, robotics competitions, and
-                                    gaming events. There are also cultural
-                                    events such as music and dance performances,
-                                    fashion, and talent shows. Interact is known
-                                    for its lively and energetic atmosphere,
-                                    with participants from different colleges
-                                    showcasing their talents and engaging in
-                                    friendly competition. The fest provides a
-                                    platform for students to showcase their
-                                    skills and creativity and helps to foster a
-                                    sense of community and collaboration among
-                                    participants.{" "}
-                                </p>
-                            </div>
-                        </div>
-                        <div className="w-full px-4 lg:w-6/12">
-                            <div className="-mx-3 flex items-center sm:-mx-4">
-                                <div className="w-full px-3 sm:px-4 xl:w-1/2">
-                                    <div className="py-3 sm:py-4">
-                                        <Image
-                                            src={image34}
-                                            alt=""
-                                            className="w-full rounded-2xl"
-                                        />
-                                    </div>
-                                    <div className="py-3 sm:py-4">
-                                        <Image
-                                            src={image5}
-                                            alt=""
-                                            className="w-full rounded-2xl"
-                                        />
-                                    </div>
-                                </div>
-                                <div className="w-full px-3 sm:px-4 xl:w-1/2">
-                                    <div className="relative z-10 my-4">
-                                        <Image
-                                            src={image42}
-                                            alt=""
-                                            className="w-full rounded-2xl"
-                                        />
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div>
+      <div className="overflow-hidden pt-20 pb-12 lg:pt-[120px] lg:pb-[90px] text-[#003366]">
+        <div className="container mx-auto">
+          {/* About Header */}
+          <div className="px-4 md:px-12 xl:px-6">
+            <div className="relative">
+              <div className="lg:w-2/3 md:text-center mx-auto">
+                <h1 className="text-[#003366] font-bold text-5xl md:text-7xl xl:text-8xl">
+                  About <span className="text-[#F4D03F]"> GAT.</span>
+                </h1>
+              </div>
             </div>
+          </div>
+
+          {/* About Content */}
+          <div className="flex flex-col lg:flex-row items-center justify-between mt-10">
+            {/* Image Section */}
+            <div className="w-full lg:w-1/2 px-4">
+              <div className="relative z-10 transition-all duration-300 rounded-2xl cursor-pointer filter">
+                <Image
+                  src={gat}
+                  alt="Global Academy of Technology"
+                  className="w-full h-[500px] object-cover rounded-2xl"
+                />
+              </div>
+            </div>
+            {/* Text Section */}
+            <div className="w-full lg:w-1/2 px-4 mt-8 lg:mt-0">
+              <div>
+                <p className="text-body-color text-justify mb-6 text-xl">
+                  Global Academy of Technology (GAT), <strong>established in 2001</strong>, is one of the most sought-after engineering and management colleges in Bengaluru, Karnataka. Nestled within a sprawling 10-acre campus, GAT provides an ideal environment for students to excel academically amidst an atmosphere of innovation and optimism.
+                </p>
+                <p className="text-body-color text-justify mb-6 text-xl">
+                  This year <strong>2025</strong> marks a momentous milestone as the institution celebrates its <strong>Silver Jubilee – 25 years</strong> of academic excellence, innovation, and transformative education. With a legacy of shaping future leaders and achievers, GAT continues to set benchmarks in higher education, solidifying its position as a premier destination for aspiring engineers and managers.
+                </p>
+                <p className="text-body-color text-justify mb-6 text-xl">
+                  GAT offers ample opportunities for various co-curricular and extracurricular activities, ensuring a well-rounded student experience. The campus is home to over <strong>3,500 students</strong> and <strong>300 experienced staff members</strong> dedicated to an effective teaching and learning process. Academics are further enriched through mentoring, peer learning, and counseling, fostering the holistic development of students. Additionally, GAT has established academic alliances with various institutions, industries, and research organizations to provide students with valuable industry perspectives.
+                </p>
+              </div>
+            </div>
+          </div>
+
+          {/* Credentials Section */}
+          <div className="px-4 md:px-12 xl:px-6 mt-16">
+            <div className="relative pb-12">
+              <div className="lg:w-2/3 md:text-center mx-auto">
+                <h1 className="text-[#003366] font-bold text-5xl md:text-7xl xl:text-8xl">
+                  Our <span className="text-[#F4D03F]">Credentials</span>
+                </h1>
+              </div>
+            </div>
+            <div className="flex flex-col md:flex-row items-start justify-between mt-10 space-y-8 md:space-y-0">
+              {/* University Affiliation */}
+              <div className="w-full md:w-1/3 px-4">
+                <div className="p-6 border border-[#003366] rounded-none text-center">
+                  {/* Credential Image */}
+                  <div className="flex justify-center">
+                    <Image
+                      src={affiliationImg}
+                      alt="University Affiliation"
+                      width={150}
+                      height={150}
+                      className="mb-4"
+                    />
+                  </div>
+                  {/* Credential Heading */}
+                  <h2 className="text-3xl font-extrabold text-[#003366] mb-2">
+                    University Affiliation
+                  </h2>
+                  {/* Credential Details */}
+                  <p className="mt-2 text-xl text-justify">
+                    The college has been affiliated with <strong className="text-[#F4D03F]">Visvesvaraya Technological University (VTU)</strong>, Belagavi, Karnataka, <strong>since 2001</strong>. This longstanding affiliation ensures our curriculum meets industry standards.
+                  </p>
+                </div>
+              </div>
+              {/* Approved by AICTE */}
+              <div className="w-full md:w-1/3 px-4">
+                <div className="p-6 border border-[#003366] rounded-none text-center">
+                  {/* Credential Image */}
+                  <div className="flex justify-center">
+                    <Image
+                      src={aicteImg}
+                      alt="Approved by AICTE"
+                      width={150}
+                      height={150}
+                      className="mb-4"
+                    />
+                  </div>
+                  {/* Credential Heading */}
+                  <h2 className="text-3xl font-extrabold text-[#003366] mb-2">
+                    Approved by AICTE
+                  </h2>
+                  {/* Credential Details */}
+                  <p className="mt-2 text-xl text-justify">
+                    All the engineering and MBA programs offered by the college have been approved by the <strong className="text-[#F4D03F]">All India Council for Technical Education (AICTE)</strong>, ensuring adherence to high education standards.
+                  </p>
+                </div>
+              </div>
+              {/* Recognized by Govt. of Karnataka */}
+              <div className="w-full md:w-1/3 px-4">
+                <div className="p-6 border border-[#003366] rounded-none text-center">
+                  {/* Credential Image */}
+                  <div className="flex justify-center">
+                    <Image
+                      src={recognizedImg}
+                      alt="Recognized by Govt. of Karnataka"
+                      width={150}
+                      height={150}
+                      className="mb-4"
+                    />
+                  </div>
+                  {/* Credential Heading */}
+                  <h2 className="text-3xl font-extrabold text-[#003366] mb-2">
+                    Recognized by Govt. of Karnataka
+                  </h2>
+                  {/* Credential Details */}
+                  <p className="mt-2 text-xl text-justify">
+                    Our programs are recognized by the <strong className="text-[#F4D03F]">Government of Karnataka</strong>, ensuring that our courses meet both regional and national benchmarks.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Footer Link */}
+          <div className="text-center mt-12 px-4">
+            <p className="text-xl">
+              For more details, visit:{" "}
+              <a
+                href="http://www.gat.ac.in"
+                className="text-[#D32F23] font-bold"
+                target="_blank"
+                rel="noreferrer"
+              >
+                www.gat.ac.in
+              </a>
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default About;
